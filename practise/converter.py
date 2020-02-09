238cc8695fa667ac78468cf5a6c468c7c13b4c77
vipinkurne <vipinkurne@gmail.com>
2020-02-09 13:50:07 +0530

    Fahereneit to Celcius Converter

--- practise/converter.py
+++ practise/converter.py
@@ -5,3 +5,8 @@ print(str(C) + ' Celcius = ' + str(F) + ' Faherenheit')
 
 x = 12
 print(x)
+
+F = input('To convert Fahereneit to Celcius, please enter number of Fahereneit: ')
+F = int(F)
+C = (F-32) * 5/9
+print(str(F) + ' Fahereneit = ' + str(C) + ' Celcius ')
