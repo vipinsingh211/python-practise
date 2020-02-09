0a4d7c3bb325eb2573d4f35d49b11d9b46658cbf
vipinkurne <vipinkurne@gmail.com>
2020-02-09 13:35:13 +0530

    This is my first commit

--- practise/converter.py
+++ practise/converter.py
@@ -2,3 +2,6 @@ C = input('To convert Celcius to Faherenheit, please enter number of Celcius: ')
 C = int(C)
 F = (C * 9/5) + 32
 print(str(C) + ' Celcius = ' + str(F) + ' Faherenheit')
+
+x = 12
+print(x)
